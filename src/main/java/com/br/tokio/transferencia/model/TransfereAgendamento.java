24d9d0d8930ef27e141140d98aa13e4dce50ed69
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-19 21:17:55 -0300

    Criação da entity TransfereAgendamento

--- src/main/java/com/br/tokio/transferencia/model/TransfereAgendamento.java
+++ src/main/java/com/br/tokio/transferencia/model/TransfereAgendamento.java
@@ -1,2 +1,79 @@
-package com.br.tokio.transferencia.model;public class TransfereAgendamento {
+package com.br.tokio.transferencia.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+
+public class TransfereAgendamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 10)
+    private String contaOrigem;
+    @Column(length = 10)
+    private String contaDestino;
+    private BigDecimal valorTransferencia;
+    private BigDecimal taxa;
+    private LocalDate dataTransferencia;
+    private LocalDate dataAgendamento;
+
+    public TransfereAgendamento() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(String contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public String getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(String contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public BigDecimal getValorTransferencia() {
+        return valorTransferencia;
+    }
+
+    public void setValorTransferencia(BigDecimal valorTransferencia) {
+        this.valorTransferencia = valorTransferencia;
+    }
+
+    public BigDecimal getTaxa() {
+        return taxa;
+    }
+
+    public void setTaxa(BigDecimal taxa) {
+        this.taxa = taxa;
+    }
+
+    public LocalDate getDataTransferencia() {
+        return dataTransferencia;
+    }
+
+    public void setDataTransferencia(LocalDate dataTransferencia) {
+        this.dataTransferencia = dataTransferencia;
+    }
+
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDate dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
 }
