4d3fc529bcb1b7a0adc02892a81a0c205813094f
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-19 21:26:15 -0300

    Criação repository TransfereAgendamentoRepository

---
+++ src/main/java/com/br/tokio/transferencia/repositories/TransfereAgendamentoRepository.java
@@ -0,0 +1,6 @@
+package com.br.tokio.transferencia.repositories;
+
+import com.br.tokio.transferencia.model.TransfereAgendamento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransfereAgendamentoRepository extends CrudRepository<TransfereAgendamento, Long> {}
