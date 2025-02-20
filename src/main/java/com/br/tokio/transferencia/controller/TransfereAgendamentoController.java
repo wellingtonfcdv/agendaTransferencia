fe21b2e418d9a85fb29a7150f3c54131476e0340
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-19 23:53:52 -0300

    Classe controller

--- src/main/java/com/br/tokio/transferencia/controller/TransfereAgendamentoController.java
+++ src/main/java/com/br/tokio/transferencia/controller/TransfereAgendamentoController.java
@@ -1,2 +1,25 @@
-package com.br.tokio.transferencia.controller;public class TransfereAgendamentoController {
+package com.br.tokio.transferencia.controller;
+
+import com.br.tokio.transferencia.model.TransfereAgendamento;
+import com.br.tokio.transferencia.services.TransfereAgendamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TransfereAgendamentoController {
+    @Autowired
+    private TransfereAgendamentoService agendamentoService;
+
+    @PostMapping
+    public TransfereAgendamento transfereAgendamento(@RequestBody TransfereAgendamento agendamento){
+        return agendamentoService.transfereAgendamento(agendamento);
+    }
+
+    @GetMapping("/agendamentos")
+    public List<TransfereAgendamento> getAgendamentos(){
+        return agendamentoService.getAgendamentos();
+    }
 }
