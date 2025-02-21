284669fe3960c84217823a574b7cb3715feb0e61
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-21 01:45:24 -0300

    Alerta erro transferência

--- src/app/agendamento/AgendamentoComponent.ts
+++ src/app/agendamento/AgendamentoComponent.ts
@@ -22,7 +22,9 @@ export class AgendamentoComponent{
             response => {
                 alert('Transferência agendada com sucesso!');
             },
-
-        )
+            error =>{
+              alert('Erro ao agendar transferência: ' + error.message);
+            }
+        );
     }
 }
