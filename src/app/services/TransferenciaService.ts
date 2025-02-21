0674c9151450541365fbf1031f256ee91bad745a
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-21 01:14:07 -0300

    Serviço para consumir a api

---
+++ src/app/services/TransferenciaService.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TransferenciaService{
+    private apiUrl = 'http://localhost:8080/transferencias';
+
+    constructor(private http: HttpClient){}
+
+    agendarTransferencia(transferencia: any): Observable<any>{
+        return this.http.post(this.apiUrl, transferencia);
+
+    }
+
+    listarTransferencias(): Observable<any>{
+        return this.http.get(this.apiUrl);
+    }
+}
