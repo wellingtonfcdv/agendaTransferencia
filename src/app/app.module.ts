0cce833d11b4325e8a0ce8cdfc836a98f324238a
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-02-21 03:16:28 -0300

    Organização dos imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AgendamentoComponent } from './agendamento/AgendamentoComponent';
 import { AppComponent } from './app.component';
+import { routes } from './app.routing';
+import { ExtratoComponent } from './extrato/ExtratoComponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendamentoComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
